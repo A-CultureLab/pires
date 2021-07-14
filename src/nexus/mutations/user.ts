a1b2ab79125a5a1a11d781b82925afe3c8a103da
hyun <coderhyun476@gmail.com>
2021-07-14 22:12:22 +0900

    updateUser

--- src/nexus/mutations/user.ts
+++ src/nexus/mutations/user.ts
@@ -1,5 +1,6 @@
 import { mutationField, nonNull, inputObjectType } from "nexus";
 import { userAuth } from "../../lib/firebase";
+import getIUser from "../../utils/getIUser";
 
 
 export const SignupInput = inputObjectType({
@@ -23,7 +24,7 @@ export const SignupInput = inputObjectType({
     }
 })
 
-export const signup = mutationField(t => t.field('signup', {
+export const signup = mutationField(t => t.nonNull.field('signup', {
     type: 'User',
     args: {
         data: nonNull(SignupInput)
@@ -39,6 +40,37 @@ export const signup = mutationField(t => t.field('signup', {
                 id,
                 ...data
             }
+        })
+    }
+}))
+
+export const UpdateUserInput = inputObjectType({
+    name: 'UpdateUserInput',
+    definition(t) {
+        t.nonNull.string('name')
+        t.nonNull.string('image')
+        t.nonNull.field('gender', { type: 'Gender' })
+        t.nonNull.field('birth', { type: 'DateTime' })
+        t.nonNull.string('addressId')
+        t.nonNull.string('address')
+        t.nonNull.string('postcode')
+        t.nonNull.float('latitude')
+        t.nonNull.float('longitude')
+        t.nullable.string('instagramId')
+        t.nonNull.string('introduce')
+    }
+})
+
+export const updateUser = mutationField(t => t.nonNull.field('updateUser', {
+    type: 'User',
+    args: {
+        data: nonNull(UpdateUserInput)
+    },
+    resolve: async (_, { data }, ctx) => {
+        const user = await getIUser(ctx)
+        return ctx.prisma.user.update({
+            where: { id: user.id },
+            data
         })
     }
 }))
