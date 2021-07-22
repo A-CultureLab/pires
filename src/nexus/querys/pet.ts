c755738449358e549358806ee79bed72f40b0716
hyun <coderhyun476@gmail.com>
2021-07-22 16:37:08 +0900

    mapPets

--- src/nexus/querys/pet.ts
+++ src/nexus/querys/pet.ts
@@ -23,26 +23,61 @@ export const CameraRegionInput = inputObjectType({
     }
 })
 
-export const PetGroupByPostCode = objectType({
-    name: 'PetGroupByPostCode',
+export const PetGroupByAddress = objectType({
+    name: 'PetGroupByAddress',
     definition(t) {
         t.nonNull.list.nonNull.field('pets', { type: 'Pet' })
         t.nonNull.int('count')
-        t.nonNull.float('latitude')
-        t.nonNull.float('longitude')
-        t.nonNull.string('postcode')
+        t.nonNull.field('address', { type: 'Address' })
     }
 })
 
-export const mapPets = queryField(t => t.nonNull.list.field('mapPets', {
-    type: PetGroupByPostCode,
+export const mapPets = queryField(t => t.nonNull.list.nonNull.field('mapPets', {
+    type: PetGroupByAddress,
     args: {
         cameraRegion: nonNull(CameraRegionInput)
     },
     resolve: async (_, { cameraRegion }, ctx) => {
 
-        const w = 0
+        const w = 10 // 가중치 deleta기반으로
+        const userGroupByAddress = await ctx.prisma.user.groupBy({
+            by: ['addressPostcode'],
+            where: {
+                address: {
+                    AND: [
+                        { latitude: { gte: cameraRegion.latitude - w } },
+                        { latitude: { lte: cameraRegion.latitude + w } },
+                        { longitude: { gte: cameraRegion.longitude - w } },
+                        { longitude: { lte: cameraRegion.longitude + w } },
+                    ],
+                }
+            },
+        })
 
-        return []
+        const petGroupByAddress = await Promise.all(
+            userGroupByAddress.map(({ addressPostcode }) =>
+                (async () => {
+                    const pets = await ctx.prisma.pet.findMany({
+                        where: { user: { addressPostcode } },
+                        orderBy: { createdAt: 'desc' }, // 신규등록한
+                        take: 2 // 두개만 
+                    })
+                    const count = await ctx.prisma.pet.count({
+                        where: { user: { addressPostcode } }
+                    })
+                    const address: any = await ctx.prisma.address.findUnique({ // address는 존재할 수 밖에 없음
+                        where: { postcode: addressPostcode }
+                    })
+
+                    return {
+                        address,
+                        count,
+                        pets
+                    }
+                })()
+            )
+        )
+
+        return petGroupByAddress.filter(v => v.pets.length > 0)
     }
 }))
