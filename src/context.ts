bb6aa515df8148f195cdb00cd96365ed72469ebf
hyun <coderhyun476@gmail.com>
2021-08-02 16:52:20 +0900

    pubsub redis

--- src/context.ts
+++ src/context.ts
@@ -1,16 +1,33 @@
 import { PrismaClient, User } from '@prisma/client'
 import { ExpressContext } from 'apollo-server-express/dist/ApolloServer'
 import { PubSub } from 'graphql-subscriptions';
-import getIUser from './utils/getIUser';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import Redis, { RedisOptions } from 'ioredis';
 
-export const pubsub = new PubSub()
+const {
+    REDIS_HOSTNAME,
+    NODE_ENV,
+} = process.env;
+
+const redisOption: RedisOptions = {
+    host: REDIS_HOSTNAME,
+    port: 6379,
+}
+
+export const pubsub = NODE_ENV === 'production'
+    ? new RedisPubSub({
+        subscriber: new Redis(redisOption),
+        publisher: new Redis(redisOption)
+    })
+    : new PubSub()
+
 export const prisma = new PrismaClient()
 
 
 export interface Context {
     prisma: PrismaClient
     expressContext: ExpressContext
-    pubsub: PubSub
+    pubsub: PubSub | RedisPubSub
     userId?: string
 }
 
