7af36cd1b4685f7954dbf1c4088e32465b4ae730
hyun <coderhyun476@gmail.com>
2021-10-12 15:21:59 +0900

    pet list

--- src/nexus/querys/pet/index.ts
+++ src/nexus/querys/pet/index.ts
@@ -1,4 +1,4 @@
-import { intArg, nonNull, nullable, queryField, stringArg } from "nexus";
+import { inputObjectType, intArg, nonNull, nullable, queryField, stringArg } from "nexus";
 import getIUser from "../../../utils/getIUser";
 import groupByAddressIdGenerator from "../../../utils/groupByAddressIdGenerator";
 
@@ -11,6 +11,42 @@ export const myPets = queryField(t => t.nonNull.list.nonNull.field('myPets', {
         return ctx.prisma.pet.findMany({
             where: { userId: user.id },
             orderBy: { orderKey: 'asc' }
+        })
+    }
+}))
+
+const PetsAdressFilterInput = inputObjectType({
+    name: 'PetsAdressFilterInput',
+    definition(t) {
+        t.nullable.string('area1Id')
+        t.nullable.string('area2Id')
+        t.nullable.string('area3Id')
+        t.nullable.string('landId')
+    }
+})
+
+export const pets = queryField(t => t.nonNull.list.nonNull.field('pets', {
+    type: 'Pet',
+    args: {
+        filter: nullable(PetsAdressFilterInput),
+        take: nullable(intArg({ default: 10 })),
+        skip: nullable(intArg({ default: 0 }))
+    },
+    resolve: async (_, { filter, skip, take }, ctx) => {
+        return ctx.prisma.pet.findMany({
+            where: filter ? {
+                user: {
+                    address: {
+                        area1Id: filter.area1Id || undefined,
+                        area2Id: filter.area2Id || undefined,
+                        area3Id: filter.area3Id || undefined,
+                        landId: filter.landId || undefined
+                    }
+                }
+            } : undefined,
+            orderBy: { updatedAt: 'desc' },
+            take: take || 0,
+            skip: skip || 0
         })
     }
 }))
